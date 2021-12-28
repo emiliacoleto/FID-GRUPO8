005c16db1235053905c307c9bee82965096398b5
manu <mgrmanu10@gmail.com>
2021-12-28 14:57:40 +0100

    Preprocesamiento de OUTLIERS COMPLETADO

--- Basico_PreprocesamientoVisualizacion.R
+++ Basico_PreprocesamientoVisualizacion.R
@@ -3,12 +3,14 @@
 
 ##Instalación de Tidyverse
 install.packages("tidyverse")
+library(tidyverse)
 
 heart <- read.csv('heart.csv')
 View(heart)
 
 ##Deteccion de outliers
 str(heart) #Contenido de heart.csv
+head(heart)
 
 
 
@@ -51,11 +53,75 @@ str(heart) #Contenido de heart.csv
 
 (heart$HeartDisease) #clasificación, 1 (sI ha tenido problemas de corazón) y 0 (NO ha tenido problemas de corazon)
 
+
+
+
 ########BOXPLOT (DIAGRAMA DE CAJAS Y BIGOTES) para la detección de outliers########
 
-boxplot(heart$Age, horizontal = TRUE)
-boxplot(heart$RestingBP, horizontal = TRUE) #Hay outliers.
-boxplot(heart$Cholesterol,  horizontal = TRUE) #hay outliers. 
-boxplot(heart$MaxHR, horizontal=TRUE)#hay 2 outliers
-boxplot(heart$Oldpeak, horizontal=TRUE)#hay outliers
+boxplot(heart2$Age, horizontal = TRUE)
+
+
+###Atributo RestingBP
+
+RestingBP <- heart$RestingBP
+
+boxplot(RestingBP, horizontal = TRUE) #Hay outliers por debajo de 92 y por encima de 170
+boxplot.stats(RestingBP)
+heart_Sin_Out_RestingBP <- RestingBP[RestingBP>92 & RestingBP<170] #Sin outliers
+boxplot(heart_Sin_Out_RestingBP, horizontal = TRUE)
+boxplot.stats(heart_Sin_Out_RestingBP)
+
+#FIltrado del dataframe mediante paquete dplyr para eliminar outliers de RestingBP
+heart1 <- filter(heart, RestingBP > 92 & RestingBP<170) #Nuevo dataframe con 875 observaciones
+
+str(heart1)
+head(heart1)
+
+
+###Atributo Cholesterol
+Cholesterol <- heart1$Cholesterol
+
+boxplot(Cholesterol, horizontal=TRUE)
+boxplot.stats(Cholesterol)
+heart1_Sin_Out_Cholesterol <- Cholesterol[Cholesterol >0 & Cholesterol < 404]
+boxplot(heart1_Sin_Out_Cholesterol, horizontal=TRUE)
+boxplot.stats(heart1_Sin_Out_Cholesterol)
+
+heart2_Sin_out_Cholesterol2 <- heart1_Sin_Out_Cholesterol[heart1_Sin_Out_Cholesterol > 110 & heart1_Sin_Out_Cholesterol< 369]
+boxplot(heart2_Sin_out_Cholesterol2, horizontal=TRUE)
+
+#FIltrado del dataframe mediante paquete dplyr para eliminar outliers de CHholesterol
+heart2 <- filter(heart1, Cholesterol > 110 & Cholesterol < 369)
+str(heart2)
+head(heart2)
+
+###Atributo MaxHR
+MaxHR <- heart2$MaxHR
+boxplot(MaxHR, horizontal=TRUE)
+boxplot.stats(MaxHR)
+
+##No es necesario el filtrado puesto que el actual dataframe(heart2) no los contiene para este atributo
+str(heart2)
+
+###Atributo OldPeak
+OldPeak <- heart2$Oldpeak
+boxplot(OldPeak, horizontal=TRUE)
+boxplot.stats(OldPeak)
+heart2_Sin_out_OldPeak <- OldPeak[OldPeak < 3.6]
+boxplot(heart2_Sin_out_OldPeak, horizontal=TRUE)
+boxplot.stats(heart2_Sin_out_OldPeak)
+
+#Filtrado del dataframe mediante paquete dplyr para eliminar outliers de Oldpeak
+heart3<- filter(heart2, OldPeak < 3.6)
+
+##Dataset reducido sin outliers
+str(heart3)
+View(heart3)
+
+boxplot(heart3$RestingBP, horizontal = TRUE)
+boxplot(heart3$Cholesterol, horizontal = TRUE)
+boxplot(heart3$MaxHR, horizontal = TRUE)
+boxplot(heart3$Oldpeak, horizontal = TRUE)
+
+
 
